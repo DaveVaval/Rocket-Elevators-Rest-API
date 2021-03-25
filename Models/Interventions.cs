6be49a8d99394a836ef409b106b737e7c758f7bd
DaveGrim <DaveGrim@users.noreply.github.com>
2021-03-25 10:29:37 -0400

    Intervention model created

---
+++ Models/Interventions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Rocket_Elevators_Rest_API.Models
+{
+    public partial class Interventions
+    {
+       public long Id { get; set; }
+       public int? Author { get; set; }
+       public int? CustomerId { get; set; }
+       public int? BuildingId { get; set; }
+       public int? BatteryId { get; set; }
+       public int? ColumnId { get; set; }
+       public int? ElevatorId { get; set; }
+       public int? EmployeeId { get; set; }
+       public DateTime? StartOfIntervention { get; set; }
+       public DateTime? EndOfIntervention { get; set; }
+       public string Result { get; set; }
+       public string Report { get; set; }
+       public string Status { get; set; }
+    }
+}
