5e8453f532e5c48276992fd301abf4a1d8a2d615
DaveGrim <DaveGrim@users.noreply.github.com>
2021-04-10 20:48:30 -0400

    Completed endpoints

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -20,7 +20,7 @@ namespace Rocket_Elevators_Rest_API.Models.Controllers
         }
         // Get Request that will return the customer email or customer id with the specified endpoint and argument
         [HttpGet("{email}/{spec}")]
-        public async Task<ActionResult<string>> CheckCustomer(string email, string spec)
+        public async Task<ActionResult<dynamic>> CheckCustomer(string email, string spec)
         {
             var _customers = await _context.Customers.ToListAsync();
 
@@ -41,5 +41,39 @@ namespace Rocket_Elevators_Rest_API.Models.Controllers
             return Ok("bruh");  
         }
 
+        // This enpoint will return a customer object with the specified email
+        [HttpGet("{email}/object")]
+        public async Task<ActionResult<Customers>> GetCustomer(string email)
+        {
+            var customers = await _context.Customers.ToListAsync();
+
+            foreach(Customers customer  in customers)
+            {
+                if(customer.CompanyContactEmail == email)
+                {
+                    return customer;
+                }
+            }
+            
+            return Ok("Invalid email!");
+        }
+
+        [HttpPut("update")]
+        public async Task<dynamic> Update([FromBody] Customers body)
+        {
+            var customer = await _context.Customers.FindAsync(body.Id);
+
+            customer.CompanyName = body.CompanyName;
+            customer.FullNameServiceTechnicalAuthority = body.FullNameServiceTechnicalAuthority;
+            customer.TechnicalAuthorityEmail = body.TechnicalAuthorityEmail;
+            customer.CompanyAddress = body.CompanyAddress;
+            customer.CompanyContactPhone = body.CompanyContactPhone;
+            customer.TechnicalAuthorityPhone = body.TechnicalAuthorityPhone;
+
+            await _context.SaveChangesAsync();
+            
+            
+            return new OkObjectResult("success");
+        }
     }
 }
